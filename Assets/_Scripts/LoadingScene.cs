81ecbb5db574f13fe5a5a33dab4ca89d87bee244
Fatma Nur Iskal <fnuriskal@gmail.com>
2024-02-01 12:22:28 +0300

    waitingsceneplus

---
+++ Assets/_Scripts/LoadingScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    private float time=0;
+  
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= 3)
+        {
+           SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
